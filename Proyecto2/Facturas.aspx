107ebed0e344ac30896a27182b7754d9ea8ae8c0
hongBinn <127228243+hongBinn@users.noreply.github.com>
2023-10-31 19:52:22 +0100

    Si

---
+++ Proyecto2/Facturas.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Facturas.aspx.cs" Inherits="Facturas" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Proyecto</title>
+    <link href="StyleMain.css" rel="stylesheet" type="text/css" />
+    <script></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Estado de factura: "></asp:Label>
+        <asp:DropDownList ID="listaEstados" runat="server">
+            <asp:ListItem Value=""></asp:ListItem>
+            <asp:ListItem Value="Rechazada">Rechazada</asp:ListItem>
+            <asp:ListItem Value="Registrada">Registrada</asp:ListItem>
+            <asp:ListItem Value="Cobrada">Cobrada</asp:ListItem>
+            <asp:ListItem Value="Emitida">Emitida</asp:ListItem>
+            <asp:ListItem Value="Anulada">Anulada</asp:ListItem>
+            <asp:ListItem Value="Aceptada">Aceptada</asp:ListItem>
+            <asp:ListItem Value="Reenviada">Reenviada</asp:ListItem>
+            <asp:ListItem Value="Anulación aceptada">Anulación aceptada</asp:ListItem>
+            <asp:ListItem Value="Solicitada anulación">Solicitada anulación</asp:ListItem>
+        </asp:DropDownList>
+        <asp:Label ID="nombreLabel" runat="server" Text="Nombre: "></asp:Label>
+        <asp:TextBox ID="filtrarNombre" runat="server" type="text"></asp:TextBox>
+        <asp:Button ID="filtrar" runat="server" Text="Filtrar" OnClick="btnFiltrar"/>
+        <asp:Button ID="restablecer" runat="server" Text="Restablecer" OnClick="Page_Reload" />
+        <div>
+            <asp:GridView ID="informacion" runat="server" AutoGenerateColumns="False"
+                        AutoGenerateEditButton="True" 
+                        OnRowEditing="informacion_RowEditing"         
+                        OnRowCancelingEdit="informacion_RowCancelingEdit" 
+                        OnRowUpdating="informacion_RowUpdating"
+                        OnPageIndexChanging="informacion_PageIndexChanging">
+                <HeaderStyle CssClass="header-row" />
+                <Columns>
+                    <asp:BoundField DataField="numeroFactura" HeaderText="Nº Factura" SortExpression="numeroFactura"/>
+                    <asp:BoundField DataField="fechaFactura" HeaderText="Fecha" SortExpression="fechaFactura" />
+                    <asp:BoundField DataField="nombre" HeaderText="Nombre Cliente" SortExpression="nombre" />
+                    <asp:BoundField DataField="apellidos" HeaderText="Apellidos de Cliente" SortExpression="apellidos" />
+                    <asp:BoundField DataField="servicio" HeaderText="Servicio" SortExpression="servicio" />
+                    <asp:BoundField DataField="CIFCliente" HeaderText="CIFCliente" SortExpression="CIFCliente" />
+                    <asp:BoundField DataField="importe" HeaderText="Importe" SortExpression="importe" />
+                    <asp:BoundField DataField="importeIVA" HeaderText="IVA" SortExpression="importeIVA" />
+                    <asp:BoundField DataField="moneda" HeaderText="Moneda" SortExpression="moneda" />
+                    <asp:BoundField DataField="estado" HeaderText="Estado" SortExpression="estado" />
+                    <asp:BoundField DataField="fechaCobro" HeaderText="Fecha de Cobro" SortExpression="fechaCobro" />
+                    <asp:TemplateField HeaderText="Edición">
+                        <ItemTemplate>
+                            <asp:LinkButton ID="lnkEdit" runat="server" CommandName="Edit" Text="Editar" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtEditNombre" runat="server" Text='<%# Bind("nombre") %>' />
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+            <asp:Button ID="commit" runat="server" OnClick="btnCommit" Text="Commit a BBDD"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Proyecto2/Facturas.aspx.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public partial class Facturas : System.Web.UI.Page
+{
+    static String cs = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+    static SqlConnection con = new SqlConnection(cs);
+    public DataTable dt = new DataTable();
+    public DataView dv = new DataView();
+
+    /**
+     * Pre:---
+     * Post: Cuando se carga la página se lee los datos de fichero .xml y meter a GridView.
+     * GetDataTableFromXml() para lee fichero .xml y se guarda en un DataTable.
+     *
+     */
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            string xmlFilePath = Server.MapPath("~/Datos/Facturas.xml");
+            dt = GetDataFromBBDD(xmlFilePath);
+            informacion.DataSource = dt;
+            informacion.DataBind();
+            Session["TaskTable"] = dt;
+        }
+    }
+    /**
+     * Pre:---
+     * Post: Cuando click a botón de restablecer se vacia los filtros y GridView devolver a su estado original.
+     *
+     */
+    protected void Page_Reload(object sender, EventArgs e)
+    {
+        string xmlFilePath = Server.MapPath("~/Datos/Facturas.xml");
+        dt = GetDataFromBBDD(xmlFilePath);
+        listaEstados.ClearSelection();
+        filtrarNombre.Text = "";
+        informacion.DataSource = dt;
+        informacion.DataBind();
+    }
+
+    /**
+     * Pre:---
+     * Post: Cuando click a botón de filtrar se lee otra vez el fichero xml,
+     * filtrar por AplicarFiltros() y meter a GridView.
+     *
+     */
+    protected void btnFiltrar(object sender, EventArgs e)
+    {
+        string xmlFilePath = Server.MapPath("~/Datos/Facturas.xml");
+        dt = GetDataFromBBDD(xmlFilePath);
+        AplicarFiltro();
+        informacion.DataSource = dt;
+        informacion.DataBind();
+    }
+
+    /**
+     * Pre:---
+     * Post: Leer los datos que tiene en el fichero xml y guardar en una DataTable.
+     *
+     */
+    private DataTable GetDataFromBBDD(string xmlFilePath)
+    {
+        DataTable dataTable = new DataTable();
+        try
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(xmlFilePath);
+            XmlNode firstNode = xmlDoc.DocumentElement.FirstChild;
+            foreach (XmlNode node in firstNode.ChildNodes)
+            {
+                dataTable.Columns.Add(node.Name, typeof(string));
+            }
+            foreach (XmlNode node in xmlDoc.DocumentElement.ChildNodes)
+            {
+                DataRow row = dataTable.NewRow();
+                foreach (XmlNode childNode in node.ChildNodes)
+                {
+                    if (!dataTable.Columns.Contains(childNode.Name))
+                        dataTable.Columns.Add(childNode.Name, typeof(string));
+                    row[childNode.Name] = childNode.InnerText;
+                }
+                dataTable.Rows.Add(row);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ocurrió un error al leer el archivo XML: " + ex.Message);
+        }
+
+        return dataTable;
+    }
+
+    /**
+     * Pre:---
+     * Post: Filtrar por datos que quiere filtrar con DataView.
+     * Se cambia los datos que estan en la DataTable por los datos filtrados.
+     *
+     */
+    private void AplicarFiltro()
+    {
+        string fNombre = filtrarNombre.Text;
+        string lEstado = listaEstados.SelectedValue;
+        dv = new DataView(dt);
+        if (!string.IsNullOrEmpty(fNombre) && !string.IsNullOrEmpty(lEstado))
+        {
+            dv.RowFilter = $"nombre LIKE '%{fNombre}%' AND estado LIKE '{lEstado}'";
+        }
+        else if (!string.IsNullOrEmpty(fNombre))
+        {
+            dv.RowFilter = $"nombre LIKE '%{fNombre}%'";
+        }
+        else if (!string.IsNullOrEmpty(lEstado))
+        {
+            dv.RowFilter = $"estado LIKE '{lEstado}'";
+        }
+        else
+        {
+            // Si no hay filtro, muestra todos los datos
+            dv.RowFilter = "";
+        }
+        dt = dv.ToTable();
+    }
+
+    /**
+     * Pre:---
+     * Post: Exportar los datos que tiene en GridView a fichero .xsl.
+     *
+     */
+    protected void btnCommit(object sender, EventArgs e)
+    {
+        Response.Clear();
+        Response.Buffer = true;
+        Response.AddHeader("content-disposition", "attachment;filename=exported_data.xls");
+        Response.Charset = "";
+        Response.ContentType = "application/vnd.ms-excel";
+        StringWriter sw = new StringWriter();
+        HtmlTextWriter hw = new HtmlTextWriter(sw);
+        AplicarFiltro();
+        GridView gvExport = new GridView();
+        gvExport.DataSource = dt;
+        gvExport.DataBind();
+        gvExport.RenderControl(hw);
+        Response.Output.Write(sw.ToString());
+        Response.Flush();
+        Response.End();
+    }
+
+    protected void informacion_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        informacion.PageIndex = e.NewPageIndex;
+        //Bind data to the GridView control.
+        BindData();
+    }
+
+    protected void informacion_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        //Set the edit index.
+        informacion.EditIndex = e.NewEditIndex;
+        //Bind data to the GridView control.
+        BindData();
+    }
+
+    protected void informacion_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        //Reset the edit index.
+        informacion.EditIndex = -1;
+        //Bind data to the GridView control.
+        BindData();
+    }
+
+    protected void informacion_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        //Retrieve the table from the session object.
+        DataTable dt = (DataTable)Session["TaskTable"];
+
+        //Update the values.
+        GridViewRow row = informacion.Rows[e.RowIndex];
+        dt.Rows[row.DataItemIndex]["importe"] = ((TextBox)(row.Cells[7].Controls[0])).Text;
+        dt.Rows[row.DataItemIndex]["apellidos"] = ((TextBox)(row.Cells[4].Controls[0])).Text;
+        dt.Rows[row.DataItemIndex]["nombre"] = ((TextBox)(row.Cells[3].Controls[0])).Text;
+
+        //Reset the edit index.
+        informacion.EditIndex = -1;
+
+        //Bind data to the GridView control.
+        BindData();
+    }
+
+    private void BindData()
+    {
+        informacion.DataSource = Session["TaskTable"];
+        informacion.DataBind();
+    }
+}
